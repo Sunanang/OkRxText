9e584dd2b711458bd706c025156a5a9aa7e748b0
Sunanang <sunanang@126.com>
2018-03-20 14:53:46 +0800

    添加注释2

--- app/src/main/java/com/sunanang/okrxtext/QNewsService.java
+++ app/src/main/java/com/sunanang/okrxtext/QNewsService.java
@@ -23,6 +23,18 @@ public interface QNewsService {
      * @Url     请求链接完整地址，直接访问
      *
      * 具体参考  https://www.jianshu.com/p/15ccdacfd199
+     *
+     *
+     *
+     注解	                    请求方式	                        用途
+     @Path	                      GET	            用于替换Url路径中的变量字符
+     @Query	                      GET	            用于拼接在Url路径后的查询参数，但相对于直接在Url后拼接，@Query则是添加一个变量
+     @QueryMap	                  GET	            效果等同于多个@Query 参数为Map类型
+     @FormUrlEncoded/@Field	      POST	            @FormUrlEncoded修饰表单域，每个表单域子件key-value采用@Field修饰
+     @Body	                      POST	            可以指定一个对象作为HTTP请求体@Body
+     @FormUrlEncoded/@Field	      PUT	            @FormUrlEncoded修饰表单域，每个表单域子件key-value采用@Field修饰
+     @HTTP/@Body	             DELETE	            @HTTP修饰表单域，以指定一个对象作为HTTP请求体@Body，此时表单域一定要写上hasBody = true
+     @Multipart/@Part或者@PartMap  POST	            文件上传使用，@Multipart修饰表单域，参数@Part修饰或者参数使用@PartMap
      */
 
     public static final String DESC = "desc"; // 指定时间之前发布的
